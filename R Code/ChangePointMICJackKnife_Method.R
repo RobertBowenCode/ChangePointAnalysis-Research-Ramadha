904efe2476ef6483bc6d08c524b3fba23edc342b
robert bowen <robertbowen.bham@gmail.com>
2022-10-06 22:31:45 -0700

    fixed the MIC file and added helpful math comments relating to each piece of code, also implemented the JackKnife MIC code, it looks like its working, but needs testing and also simulations done

---
+++ R Code/ChangePointMICJackKnife_Method.R
@@ -0,0 +1,84 @@
+# testing and locating changes in variance of a process with independent data using the MIC method with JackKnife Likelyhood  
+#
+#
+#
+#Author Robert Bowen 10/6/22
+#
+#MIC Change Point Method for parametric Exponential Distribution
+# 
+# 
+
+
+
+
+
+#MIC Change Point Method for parametric Exponential Distribution with JackKnife likelyhood applied
+
+
+
+findChangesExponentialMICJackKnife <- function(seq, nom_alpha){ # This should be O(n^2) pretty slow, simulations will take a lot of time unfortunately. 
+  #seq is the sequence that is being determined to have a change point or not
+  #nom_alpha is the nominal alpha that we would like to use in our hypothesis test
+  
+  #variables
+  MICJa = NULL
+  MICJ_probs_alt = NULL
+  MICJ_probs_null = NULL
+  
+  for(j in 1:length(seq)) #apply the JackKnife Method to the alternative likeylhood
+  {
+    
+    jackknife_seq = seq[-c(j)] #remove one value
+    
+    n = length(jackknife_seq)
+    mu = mean(jackknife_seq)
+  
+    for(i in 2:(n-2)){ #calculate the MIC model at each changepoint i for a removed value of j
+      
+      
+      #parameter estimations for theta_1 and theta_2
+      lambda1 =i/sum(seq[1:i]) 
+      r=i+1
+      lambda2= (n-i) / sum(seq[r:n]) #calc the estimate for the 2nd dist
+      
+      
+      MICJa[i]= -2*(i*log(lambda1) -lambda1*sum(jackknife_seq[1:i]) +(n-i)*log(lambda2)  -lambda2*sum(jackknife_seq[r:n]) ) + 2*log(n) + ((2*i/n-1)^2)*log(n) 
+    }           #log likelyhood -2*(L(theta1,theta2,k))                             +         #complexity(ˆθ1k,ˆθ2k, k) = 2dim(ˆθ1k) + (2k/ n − 1)^2 (log(n)), where dim(ˆθ1k) is one
+      
+    MICJ_probs_alt[j] = min(MICJa, na.rm = TRUE) #store the min for this removed value of k
+  }
+  
+  
+  for(j in 1:length(seq)) #apply the JackKnife Method to the null log likelyhood
+  {
+    jackknife_seq = seq[-c(j)] #remove one value
+    
+    n = length(jackknife_seq)
+    mu = mean(jackknife_seq)
+    lambda_n = 1/mu
+    
+    MICJ_probs_null[j] = -2*(n*log(lambda_n) - (lambda_n)*sum(seq[1:n])) + log(n)*1
+  }
+  
+  
+  MIC_alt <-(mean(MICJ_probs_alt)) #take the average of the minimized models for Jackknife
+  MIC_null <- mean(MICJ_probs_null)  #take the jacknife model for the null hypothesis
+  
+  
+  return(ifelse(MIC_null - MIC_alt + log(n)>qchisq(1 - nom_alpha,3), 1, 0))
+  #Sn = MIC(n) − min( 1≤k<n MIC(k)) + dim(θ) log n
+  #Sn follows a chi square distribution with d degrees of freedom where d is the dimension of theta
+}
+
+
+#testing out simulations for type one error to see how well it performs 
+n=500
+repetition =200
+count = NULL
+for(i in 1:repetition){
+  x=rexp(n,1)
+  count[i] = findChangesExponentialMICJackKnife(x, 0.05)
+  
+}
+
+mean(count)
--- R Code/ChangePointMIC_Method.R
+++ R Code/ChangePointMIC_Method.R
@@ -1,32 +1,41 @@
 # testing and locating changes in variance of a process with independent data using the MIC method 
-
-
-
-
-
-
+#
+#
+#
+#Author Robert Bowen 10/6/22
+#
 #MIC Change Point Method for parametric Exponential Distribution
+# 
+# 
 
-findChangesExponentialMIC <- function(seq){
-  #seq is the sequence we're checking for changes in
-  #our type 1 error is 0.05
+
+findChangesExponentialMIC <- function(seq, nom_alpha){
+  #seq is the sequence that is being determined to have a change point or not
+  #nom_alpha is the nominal alpha that we would like to use in our hypothesis test
+  
+  #variables
   MICa = NULL
   n = length(seq)
   mu = mean(seq)
+  lambda_n = 1/mu
   
-  for(i in 2:(n-2)){
-    #MIC method for exponential distributions
-    lambda_1 =i/sum(x[1:i])
+  for(i in 2:(n-2)){ #calculate the MIC model at each changepoint i
+    
+    
+    #parameter estimations for theta_1 and theta_2
+    lambda1 =i/sum(seq[1:i]) 
     r=i+1
-    lambda2= (n-i) / sum(x[r:n])
-    MICa[i]= -2*(i*log(lambda1) -lambda1*sum(x[1:i]) +(n-i)*log(lambda2)  -lambda2*sum(x[r:n])) +((2*i/n-1)^2)*log(n)
-  }
+    lambda2= (n-i) / sum(seq[r:n]) #calc the estimate for the 2nd dist
+
+    
+    MICa[i]= -2*(i*log(lambda1) -lambda1*sum(seq[1:i]) +(n-i)*log(lambda2)  -lambda2*sum(seq[r:n]) ) + 2*log(n) + ((2*i/n-1)^2)*log(n) 
+  }           #log likelyhood -2*(L(theta1,theta2,k))                             +         #complexity(ˆθ1k,ˆθ2k, k) = 2dim(ˆθ1k) + (2k/ n − 1)^2 (log(n)), where dim(ˆθ1k) is one
   
-  MIC_alt <-(min(MICa, na.rm = TRUE))
-  MIC_null <- n*log(2*pi)+n*log(sum((x-mu)^2)/n)+n+1*log(n)
+  MIC_alt <-(min(MICa, na.rm = TRUE)) #take the best model out of each changepoint
+  MIC_null <- -2*(n*log(lambda_n) - (lambda_n)*sum(seq[1:n])) + log(n)*1  #take the model for the null hypothesis
   
   
-  return(ifelse(MICo - MICk +2*log(n)>qchisq(0.95,2), 1, 0))
+  return(ifelse(MIC_null - MIC_alt + log(n)>qchisq(1 - nom_alpha,3), 1, 0))
+  #Sn = MIC(n) − min( 1≤k<n MIC(k)) + dim(θ) log n
+  #Sn follows a chi square distribution with d degrees of freedom where d is the dimension of theta(why is this three here again? shouldn't it be 1, but then it doesn't work strange)
 }
-
-
