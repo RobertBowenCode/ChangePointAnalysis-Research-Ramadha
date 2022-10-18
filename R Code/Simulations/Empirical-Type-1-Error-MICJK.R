cdfbf8fd37c54365f1fdbae026a1143d0e9cb2dc
robert bowen <robertbowen.bham@gmail.com>
2022-10-18 15:17:55 -0700

    added Empirical type 1 error simulations for MIC method

--- R Code/Simulations/Empirical-Type-1-Error-MICJK.R
+++ R Code/Simulations/Empirical-Type-1-Error-MICJK.R
@@ -149,9 +149,118 @@ for (sizes in sample_sizes) #for each sample size
 
 
 
+##################Method of Hypothesis test ####################################
 
 
 
+#MIC Change Point Method for parametric Exponential Distribution with likelyhood applied
+
+
+
+
+findChangesExponentialMIC <- function(seq, nom_alpha){
+  #seq is the sequence that is being determined to have a change point or not
+  #nom_alpha is the nominal alpha that we would like to use in our hypothesis test
+  
+  #variables
+  MICa = NULL
+  n = length(seq)
+  mu = mean(seq)
+  lambda_n = 1/mu
+  
+  for(i in 2:(n-2)){ #calculate the MIC model at each changepoint i
+    
+    
+    #parameter estimations for theta_1 and theta_2
+    lambda1 =i/sum(seq[1:i]) 
+    r=i+1
+    lambda2= (n-i) / sum(seq[r:n]) #calc the estimate for the 2nd dist
+    
+    
+    MICa[i]= -2*(i*log(lambda1) -lambda1*sum(seq[1:i]) +(n-i)*log(lambda2)  -lambda2*sum(seq[r:n]) ) + 2*log(n) + ((2*i/n-1)^2)*log(n) 
+  }           #log likelyhood -2*(L(theta1,theta2,k))                             +         #complexity(ˆθ1k,ˆθ2k, k) = 2dim(ˆθ1k) + (2k/ n − 1)^2 (log(n)), where dim(ˆθ1k) is one
+  
+  MIC_alt <-(min(MICa, na.rm = TRUE)) #take the best model out of each changepoint
+  MIC_null <- -2*(n*log(lambda_n) - (lambda_n)*sum(seq[1:n])) + log(n)*1  #take the model for the null hypothesis
+  
+  
+  return(ifelse(MIC_null - MIC_alt + log(n)>qchisq(1 - nom_alpha,3), 1, 0))
+  #Sn = MIC(n) − min( 1≤k<n MIC(k)) + dim(θ) log n
+  #Sn follows a chi square distribution with d degrees of freedom where d is the dimension of theta(why is this three here again? shouldn't it be 1, but then it doesn't work strange)
+}
+
+
+
+
+#ChangePoint Simulation with Exp(1) on MIC  Method
+#simulating to calc empirical type 1 error at various sample sizes and nominal values of alpha
+
+#repetitions =500
+# testing at nominal alphas = 0.01, 0.05, 0.1
+
+###Variables###
+exp_mean = 1
+
+#Store the results
+simulations_n_50  = c()
+simulations_n_100 = c()
+simulations_n_150 = c()
+simulations_n_200 = c()
+
+
+
+sample_sizes = c(50,100,150,200)
+nominal_type_1_error = c(0.01, 0.05, 0.1)
+repetitions = 500
+
+
+for (sizes in sample_sizes) #for each sample size
+{
+  
+  array_index = 1
+  
+  
+  for(type_1 in nominal_type_1_error) #calculate the empirical type 1 error at nominal values of 0.01, 0.05, and 0.1
+  {
+    
+    
+    empirical_type_1 = 0
+    count = 0
+    
+    for(i in 1:repetitions)
+    {
+      seq <-rexp(sizes, exp_mean) #simulate an exponential dataset with no changes
+      count = count + findChangesExponentialMIC(seq, type_1) #run test on dataset to check for change
+    }
+    
+    empirical_type_1 = count/repetitions
+    
+    
+    #store the results
+    if(sizes == 50)
+    {
+      simulations_n_50[array_index] = empirical_type_1
+    }
+    else if(sizes == 100)
+    {
+      simulations_n_100[array_index] = empirical_type_1
+    }
+    else if (sizes == 150)
+    {
+      simulations_n_150[array_index] = empirical_type_1
+    }
+    else if (sizes == 200)
+    {
+      simulations_n_200[array_index] = empirical_type_1
+      
+    }
+    
+    array_index = array_index+1
+    
+  }
+  
+  
+}
 
 
 
